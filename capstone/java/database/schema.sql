77112c2e5a2ba21cd05faf99976f44c4db46325b
Madeline Akers <madeline.akers@gmail.com>
2022-09-26 15:19:26 -0500

    created user_profile table

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, user_profile;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -8,6 +8,15 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE user_profile (
+	user_id int NOT NULL,
+	activities varchar,
+	cycling_team varchar,
+	user_weight int,
+	user_age int,
+	CONSTRAINT PK_user_id PRIMARY KEY (user_id)
 );
 
 COMMIT TRANSACTION;
