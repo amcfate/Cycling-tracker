57a371d17101bdd52ba16c0f68a5353bb0895ad1
isaac <isaacmlopez@hotmail.com>
2022-09-26 17:17:11 -0400

    Added frontend map functionality. Still working on rendering.

--- capstone/vue/src/App.vue
+++ capstone/vue/src/App.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="app">
+    <Map />
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link v-bind:to="{name: 'Map' }">Map Test!</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+import Map from "@/components/Map";
+export default {
+  name: 'App',
+  components: {
+    Map
+  }
+}
+</script>
--- capstone/vue/src/main.js
+++ capstone/vue/src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
-
+import 'vuejs-google-maps/dist/vuejs-google-maps.css'
+import Map from "@/components/Map";
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
 
 new Vue({
+  Map,
   router,
   store,
   render: h => h(App)
