70b1a04bcace274f37ee4b63ea5e89bf174d1953
Andrew McFate <andrew.mcfate@gmail.com>
2022-09-27 17:22:25 -0400

    started activities component

--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -1,17 +1,30 @@
 <template>
+<div>
+  <activities>
+    <Map/>
+  </activities>
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
-    <Map />
+    
+  </div>
   </div>
 </template>
 
 <script>
 import Map from "@/components/maps/Map";
+import Activities from '../components/tiles/activities.vue';
 export default {
   name: "home",
   components :{
-    Map
+    Map,
+    Activities
   }
 };
 </script>
+
+<style>
+#Map{
+  opacity: .05;
+}
+</style>
