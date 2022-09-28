babadf98fa1cc5eb420c281aa2a9e1a4e6dac240
isaac <isaacmlopez@hotmail.com>
2022-09-28 10:19:13 -0400

    Help me

--- capstone/vue/src/components/maps/Map.vue
+++ capstone/vue/src/components/maps/Map.vue
@@ -1,45 +1,102 @@
-<template>
-  <app-map-loader
-      :mapConfig="mapConfig"
-      apiKey=""
-      class="gmap">
-    <template slot-scope="{ google, map }">
-      <app-polyline
-          :polylines="polylines"
-          :google="google"
-          :map="map">
-      </app-polyline>
-    </template>
-  </app-map-loader>
-</template>
-
 <script>
-import MapLoader from './MapLoader';
-import Polyline from './Polyline'
-import { mapSettings } from '../../../constants/mapSettings';
+/* eslint-disable no-undef */
+import { computed, ref, onMounted, onUnmounted, watch } from 'vue'
+import { useGeolocation } from './useGeolocation'
+import { Loader } from '@googlemaps/js-api-loader'
+
+const GOOGLE_MAPS_API_KEY = ' AIzaSyDjB5lrCyoXaoU7Lv4RXi909TRAq5Wua9g'
 
 export default {
-  components: {
-    appMapLoader: MapLoader,
-    appPolyline: Polyline
-  },
-  props: {
-    polylines: {
-      type: Array,
-      default: () => []
+  name: 'Map',
+  setup() {
+    const { coords } = useGeolocation()
+    const currPos = computed(() => ({
+      lat: coords.value.latitude,
+      lng: coords.value.longitude
+    }))
+    const otherPos = ref(null)
+
+    const loader = new Loader({ apiKey: GOOGLE_MAPS_API_KEY })
+    const mapDiv = ref(null)
+    let map = ref(null)
+    let clickListener = null
+    onMounted(async () => {
+      await loader.load()
+      map.value = new google.maps.Map(mapDiv.value, {
+        center: currPos.value,
+        zoom: 7
+      })
+      clickListener = map.value.addListener(
+          'click',
+          ({ latLng: { lat, lng } }) =>
+              (otherPos.value = { lat: lat(), lng: lng() })
+      )
+    })
+    onUnmounted(async () => {
+      if (clickListener) clickListener.remove()
+    })
+
+    let line = null
+    watch([map, currPos, otherPos], () => {
+      if (line) line.setMap(null)
+      if (map.value && otherPos.value != null)
+        line = new google.maps.Polyline({
+          path: [currPos.value, otherPos.value],
+          map: map.value
+        })
+    })
+
+    const haversineDistance = (pos1, pos2) => {
+      const R = 3958.8 // Radius of the Earth in miles
+      const rlat1 = pos1.lat * (Math.PI / 180) // Convert degrees to radians
+      const rlat2 = pos2.lat * (Math.PI / 180) // Convert degrees to radians
+      const difflat = rlat2 - rlat1 // Radian difference (latitudes)
+      const difflon = (pos2.lng - pos1.lng) * (Math.PI / 180) // Radian difference (longitudes)
+
+      const d =
+          2 *
+          R *
+          Math.asin(
+              Math.sqrt(
+                  Math.sin(difflat / 2) * Math.sin(difflat / 2) +
+                  Math.cos(rlat1) *
+                  Math.cos(rlat2) *
+                  Math.sin(difflon / 2) *
+                  Math.sin(difflon / 2)
+              )
+          )
+      return d
     }
-  },
-  computed: {
-    mapConfig() {
-      return {
-        ...mapSettings,
-        center: { lat: 0, lng: 0 }
-      };
-    }
+    const distance = computed(() =>
+        otherPos.value === null
+            ? 0
+            : haversineDistance(currPos.value, otherPos.value)
+    )
+    return { currPos, otherPos, distance, mapDiv }
   }
-};
+}
 </script>
 
-<style>
+<template>
+  <div class="d-flex text-center" style="height: 20vh">
+    <div class="m-auto">
+      <h4>Your Position</h4>
+      Latitude: {{ currPos.lat.toFixed(2) }}, Longitude:
+      {{ currPos.lng.toFixed(2) }}
+    </div>
+    <div class="m-auto">
+      <h4>Distance</h4>
+      {{ distance.toFixed(2) }} miles
+    </div>
+    <div class="m-auto">
+      <h4>Clicked Position</h4>
+      <span v-if="otherPos">
+          Latitude: {{ otherPos.lat.toFixed(2) }}, Longitude:
+          {{ otherPos.lng.toFixed(2) }}
+        </span>
+      <span v-else>Click the map to select a position</span>
+      <div ref="mapDiv" style="width: 100%; height: 80vh" />
+    </div>
+  </div>
 
-</style>
+</template>
