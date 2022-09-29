2f7d2df55fef9f6da422ed590dc3f50f4c823765
Madeline Akers <madeline.akers@gmail.com>
2022-09-29 11:20:41 -0500

    added time and date to activity in db

--- capstone/java/src/main/java/com/techelevator/dao/TrackpointDao.java
+++ capstone/java/src/main/java/com/techelevator/dao/TrackpointDao.java
@@ -2,6 +2,7 @@ package com.techelevator.dao;
 
 import com.techelevator.model.Trackpoint;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface TrackpointDao {
@@ -9,6 +10,7 @@ public interface TrackpointDao {
     List<Trackpoint> findAllTrackpoints(); //would we ever really need this? If we want to display
     //all routes? would that mess up the routes?
 
+   List<Trackpoint> findTrackpointsByRouteId(int routeId);
     //get all trackpoints by route id
 
     //get a single trackpoint?
@@ -16,6 +18,13 @@ public interface TrackpointDao {
     //get a beginning trackpoint?
 
     //get an end trackpoint?
+
+    //get a trackpoint by latitude/longitude?
+
+    boolean create(int routeId, BigDecimal latitude, BigDecimal longitude, double elevation);
+    //to create a new route, you'll need to create a series of new trackpoints with the same route ID
+    //not sure how to handle that, maybe when you create a route it calls this and gives all
+    //the trackpoints created the same route id?
 
 
 
