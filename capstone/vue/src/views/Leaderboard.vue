246662209c3b19d924a15829133735ddf1c4cbd7
Madeline Akers <madeline.akers@gmail.com>
2022-10-03 08:21:57 -0500

    beginnings of a leaderboard page

---
+++ capstone/vue/src/views/Leaderboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>  
+    <overall-miles-leaderboard/>
+</div>
+</template>
+
+<script>
+import overallMilesLeaderboard from "../components/maps/overallMilesLeaderboard.vue";
+export default {
+  name: "Leaderboard",
+  data(){},
+  components: {
+    overallMilesLeaderboard,
+  }
+
+}
+</script>
+
+<style>
+
+</style>
