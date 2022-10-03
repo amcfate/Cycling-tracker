93155510bfc7420b38b689b203e0e5ba54bfc91c
isaac <isaacmlopez@hotmail.com>
2022-10-03 08:56:05 -0400

    Merge from main

--- capstone/java/src/main/java/com/techelevator/controller/RouteController.java
+++ capstone/java/src/main/java/com/techelevator/controller/RouteController.java
@@ -2,27 +2,100 @@ package com.techelevator.controller;
 
 import com.techelevator.dao.RouteDao;
 import com.techelevator.dao.TrackpointDao;
+import com.techelevator.model.NewRouteDTO;
+import com.techelevator.model.NewTrackpointDTO;
+import com.techelevator.model.Route;
 import com.techelevator.model.Trackpoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@CrossOrigin
+
+
+
+@PreAuthorize("isAuthenticated()")
+
 public class RouteController {
 
+    @Autowired
     private TrackpointDao trackpointDao;
+    @Autowired
     private RouteDao routeDao;
 
-    //where do I want to route a trackpoint? you don't actually ever wants to get a trackpoint
-    //by itself probably.... or do you you
-    //but maybe build this method just to test that I can get a trackpoint since getRoute will
-    //have to call get trackpoint? No one is ever going to know a trackpoint id or route id,
-    //but if they click on a route, do I want it to give them the lat/long/elev
+    //GET methods
 
-//    @RequestMapping(value = "/getTrackpoint/{id}", method = RequestMethod.GET)
-//    public
+    @RequestMapping(value = "/gettrackpoint/{id}", method = RequestMethod.GET)
+    public Trackpoint getTrackpointById (@PathVariable int id){
+        return trackpointDao.getTrackpointById(id);
+    }
 
-}
+    @RequestMapping(value = "gettrackpoint/route/{id}", method = RequestMethod.GET)
+    public List <Trackpoint> getTrackpointsByRouteId(@PathVariable int id) {
+        return trackpointDao.findTrackpointsByRouteId(id);
+    }
+
+    @RequestMapping(value = "/getalltrackpoints", method = RequestMethod.GET)
+    public List <Trackpoint> getAllTrackpoints(){
+        return trackpointDao.findAllTrackpoints();
+    }
+
+    @RequestMapping(value = "/getallroutes", method = RequestMethod.GET)
+    public List <Route> getAllRoutes(){
+        return routeDao.findAllRoutes(); //getting a null pointer exception in IntelliJ when I try to get in postman
+    }
+
+    @RequestMapping(value = "/getroute/{id}", method = RequestMethod.GET)
+    public Route getRouteById(@PathVariable int id) {
+        return routeDao.findRouteById(id);
+    }
+
+    //POST methods
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(value = "/savetrackpoint", method = RequestMethod.POST)
+    public Trackpoint createTrackpoint(@Valid @RequestBody NewTrackpointDTO newTrackpointDTO){ //what does Valid do here, or what would lack of Valid do -- trackpoints are passed as an object {lat: 111.111, lng: 111.111} so you would save lat and long as one trackpoint and assign it a route ID
+        Trackpoint newTrackpoint = buildTrackpointFromTrackpointDTO(newTrackpointDTO);
+        newTrackpoint = trackpointDao.addTrackpoint(newTrackpoint);
+        return newTrackpoint;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(value = "/saveroute", method = RequestMethod.POST) // -- A route would simply be a group of trackpoints
+    public Route createRoute(@Valid @RequestBody NewRouteDTO newRouteDTO){
+        Route newRoute =buildRouteFromRouteDTO(newRouteDTO);
+        newRoute = routeDao.addRoute(newRoute);
+        return newRoute;
+    }
+
+   //Other methods
+
+    private Trackpoint buildTrackpointFromTrackpointDTO(NewTrackpointDTO trackpointDTO){
+        return new Trackpoint(trackpointDTO.getRouteId(),
+                              trackpointDTO.getTrackpointId(),
+                              trackpointDTO.getLatitude(),
+                              trackpointDTO.getLongitude(),
+                              trackpointDTO.getElevation());
+    }
+
+    private Route buildRouteFromRouteDTO(NewRouteDTO routeDTO){
+        return new Route(routeDTO.getRouteId(), routeDTO.getRouteName(), routeDTO.getDescription(),
+                routeDTO.getDistanceMiles(), routeDTO.getElevation(), routeDTO.getAscent());
+    }
+
+
+ }
+
+
+
+
